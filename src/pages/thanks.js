15feb617b6c2c9fc452b48342c3284c3d8b0b9a7
henridev <henri.dbel@gmail.com>
2020-06-07 09:24:22 +0200

    'deployment'

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,9 +1,35 @@
 import React from "react"
 import Layout from "../components/global/Layout"
+import { Link } from "gatsby"
 
 export default () => (
   <Layout>
-    <h1>Thank you!</h1>
-    <p>This is a custom thank you page for form submissions</p>
+    <div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+      <div class="flex flex-col w-full lg:w-1/2 justify-center items-start pt-12 pb-24 px-6">
+        <p class="uppercase tracking-loose">die is verzonden</p>
+        <h1 class="font-bold text-3xl my-4">Bedankt voor u bericht</h1>
+        <p class="leading-normal mb-4">
+          Wij contacteren u zodra we u bericht ontvangen
+        </p>
+
+        <button class="font-semibold m-3 text-red-dark bg-white hover:bg-grey  hover: rounded shadow hover:shadow-lg py-2 px-4   hover:border-grey">
+          <Link to={"/"}>terug naar home</Link>
+        </button>
+      </div>
+      <div class="w-full lg:w-1/2 lg:py-6 text-center">
+        <div
+          style={{
+            backgroundImage: `Url(/assets/svg/correct.svg)`,
+            width: "300px",
+            height: "300px",
+            backgroundRepeat: "no-repeat",
+            justifySelf: "center",
+            margin: "2rem auto",
+            backgroundSize: "cover",
+          }}
+          className="fill-current text-gray-900 w-3/5 mx-auto"
+        ></div>
+      </div>
+    </div>
   </Layout>
 )
